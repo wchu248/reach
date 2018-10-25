bb8e435389b7fca25c991867f4711ae7f57658c4
Pavan Gollapalli <pavankgollapalli@gmail.com>
2018-10-25 12:01:44 -0400

    Added relationships, need to validate/define class methods

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  belongs_to :goal
+  belongs_to :user
+
+  #validate task duration
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_many :goals
+  has_many :tasks
+
+  #validate email and phone number
 end
